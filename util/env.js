c398844e07e74aaa68e05de30c0b2063f1f74a33
metteec2 <chmettee@gmail.com>
2021-08-23 12:12:54 -0400

    updated apiUrl link to reference our beanstalk

--- util/env.js
+++ util/env.js
@@ -2,5 +2,5 @@ const APP_ROOT = document.getElementById('app-root');
 
 export default {
     rootDiv: APP_ROOT,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'Lunacharlesapi-env.eba-mkbkdqcy.us-east-1.elasticbeanstalk.com'
 }
