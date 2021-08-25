f95c0db0974e9470bc1150c3665e4492673a64a9
LunaHaines <nhaines95@gmail.com>
2021-08-25 15:05:00 -0600

    .js

--- components/studentlogin/studentlogin.component.js
+++ components/studentlogin/studentlogin.component.js
@@ -1,4 +1,4 @@
-import { ViewComponent } from "../view.component";
+import { ViewComponent } from "../view.component.js";
 import env from '../../util/env.js';
 import state from '../../util/state.js';
 import router from '../../app.js';
