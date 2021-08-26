fc8dee54a700cf3c39d9c49ce46d332cf82bc6fa
LunaHaines <nhaines95@gmail.com>
2021-08-26 08:46:58 -0600

    first attempt at student dashboard get

---
+++ components/studentdashboard/studentdashboard.component.js
@@ -0,0 +1,55 @@
+import { ViewComponent } from "../view.component.js";
+import env from '../../util/env.js';
+import state from '../../util/state.js';
+import router from '../../app.js';
+
+StudentDashboard.prototype = new ViewComponent('studentdashboard')
+function StudentDashboard() {
+
+    let infoButtonElement;
+    let courseButtonElement;
+    let registerButtonElement;
+    let unregisterButtonElement;
+    let infoParagraph;
+
+    function updateInfo(info) {
+        if (info) {
+            infoParagraph.removeAttribute('hidden');
+            infoParagraph.innerText = info;
+        } else {
+            infoParagraph.setAttribute('hidden', 'true');
+            infoParagraph.innerText = '';
+        }
+    }
+
+    async function showInfo() {
+        try {
+            let resp = await fetch(`${env.apiUrl}/registration`, {
+                method='GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `${state.authUser.token}`
+                }
+            });
+
+            let queryResult = await resp.json();
+
+            updateInfo(queryResult);
+        }
+    }
+
+    this.render() {
+        StudentDashboard.prototype.injectTemplate(() => {
+            infoButtonElement = document.getElementById('student-info-button');
+            courseButtonElement = document.getElementById('student-course-button');
+            registerButtonElement = document.getElementById('student-register-button');
+            unregisterButtonElement = document.getElementById('student-unregister-button');
+            infoParagraph = document.getElementById('student-query-result');
+
+            infoButtonElement.addEventListener('click', showInfo);
+            courseButtonElement.addEventListener('click', showCourses);
+            registerButtonElement.addEventListener('click', showRegisterForm);
+            unregisterButtonElement.addEventListener('click', showUnregisterForm);
+        })
+    }
+}
