b386c20eb68b90803127bbf4846c496cc2bd77c4
LunaHaines <nhaines95@gmail.com>
2021-08-27 07:31:04 -0600

    showCourses in progress

--- components/studentdashboard/studentdashboard.component.js
+++ components/studentdashboard/studentdashboard.component.js
@@ -15,15 +15,15 @@ function StudentDashboard() {
     function updateInfo(info) {
         if (info) {
             infoContainerElement.removeAttribute('hidden');
-            infoContainerElement.innerHtml = info;
+            infoContainerElement.innerHTML = '';
+            infoContainerElement.innerHTML = info;
         } else {
             infoContainerElement.setAttribute('hidden', 'true');
-            infoContainerElement.innerHtml = '';
+            infoContainerElement.innerHTML = '';
         }
     }
 
     async function showInfo() {
-        console.log('this should work...');
         try {
             let resp = await fetch(`${env.apiUrl}/student`, {
                 headers: {
@@ -35,6 +35,7 @@ function StudentDashboard() {
             let queryResult = await resp.json();
 
             let newHtml = `
+            <br>
             <h3>User Info</h3>
             <p>name: ${queryResult[0].firstName} ${queryResult[0].lastName}</p>
             <p>email: ${queryResult[0].email}</p>
@@ -42,6 +43,7 @@ function StudentDashboard() {
             <table class="table table-hover">
                 <thead>
                     <tr>
+                        <th scope="col">#</th>
                         <th scope="col">Number</th>
                         <th scope="col">Name</th>
                         <th scope="col">Description</th>
@@ -50,28 +52,58 @@ function StudentDashboard() {
                 </thead>
                 <tbody>`
 
-                for (let i = 0; i < queryResult.length; i++) {
-                    if (i != 0) {
-                        newHtml += `
-                            <tr>
-                                <td>${queryResult[i].number}</td>
-                                <td>${queryResult[i].name}</td>
-                                <td>${queryResult[i].description}</td>
-                                <td>${queryResult[i].professor}</td>
-                            </tr>`
-                    }
+                for (let i = 1; i < queryResult.length; i++) {
+                    newHtml += `
+                        <tr>
+                            <th>${i}</h>
+                            <td>${queryResult[i].number}</td>
+                            <td>${queryResult[i].name}</td>
+                            <td>${queryResult[i].description}</td>
+                            <td>${queryResult[i].professor}</td>
+                        </tr>`
                 }
 
                 newHtml += `
                 </tbody>
                 </table>`
-                console.log(newHtml);
             
             updateInfo(newHtml);
         } catch (error) {
             console.error(error);
         }
     }
+
+    async function showCourses() {
+        try {
+            let resp = await fetch(`${env.apiUrl}/registration`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `${state.authUser.token}`
+                }
+            });
+
+            let courses = await resp.json();
+
+            newHtml = `
+            <br>
+            <h3>Course List</h3>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Number</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Professor</th>
+                        <th scope="col">Capacity</th>
+                        <th scope="col">Registered</th>
+                    </tr>
+                </thead>
+                <tbody>`
+        }
+    }
+
+
 
     this.render = function() {
         StudentDashboard.prototype.injectTemplate(() => {
@@ -81,8 +113,10 @@ function StudentDashboard() {
             unregisterButtonElement = document.getElementById('student-unregister-button');
             infoContainerElement = document.getElementById('show-info-container');
 
+            showInfo();
+
             infoButtonElement.addEventListener('click', showInfo);
-            //courseButtonElement.addEventListener('click', showCourses);
+            courseButtonElement.addEventListener('click', showCourses);
             //registerButtonElement.addEventListener('click', showRegisterForm);
             //unregisterButtonElement.addEventListener('click', showUnregisterForm);
         });
